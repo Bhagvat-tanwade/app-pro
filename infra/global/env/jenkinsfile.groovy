8f4ed904c89ccf47edeee6429344de1260adaac9
Bhagvat-tanwade <tanwadebhagvat013@gmail.com>
2026-09-14 15:53:55 +0530

    Refactor Jenkinsfile for Terraform stages and parameters

--- infra/global/env/jenkinsfile.groovy
+++ infra/global/env/jenkinsfile.groovy
@@ -17,145 +17,97 @@ pipeline {
         )
     }
 
-
     stages {
 
         stage('PULL') {
             steps {
-                git branch: 'dev', url: 'https://github.com/Bhagvat-tanwade/app-pro.git'
+                git branch: 'dev',
+                    url: 'https://github.com/Bhagvat-tanwade/app-pro.git'
             }
         }
 
+        stage('Terraform Init') {
+            steps {
+                withCredentials([
+                    [$class: 'AmazonWebServicesCredentialsBinding',
+                     credentialsId: 'aws-credentials']
+                ]) {
 
-        // stage('TERRAFORM INIT') {
-        //     steps {
-        //         sh '''
-        //             cd infra/global/env/${ENVIRONMENT}
-        //             terraform init
-        //         '''
-        //     }
-        // }
+                    sh """
+                        cd infra/global/env/${params.ENVIRONMENT}
 
-
-        // stage('Terraform Init') {
-        //     steps {
-        //         withCredentials([
-        //             [$class: 'AmazonWebServicesCredentialsBinding',
-        //              credentialsId: 'aws-credentials']
-        //         ]) {
-
-        //             sh """
-        //                 cd infra/global/env/${ENVIRONMENT}
-
-        //                 cat > backend.tf << EOF
-        //             terraform {
-        //             backend "s3" {
-        //                 bucket         = "terraform-state-bucket-cbz-kharadi-4"
-        //                 key            = "global/${params.ENVIRONMENT}/terraform-global.tfstate"
-        //                 region         = "ap-south-1"
-        //                 encrypt        = true
-        //                 dynamodb_table = "terraform-state-lock"
-        //                 use_lockfile   = true
-        //             }
-        //             }
-        //             EOF
-
-        //                 rm -rf .terraform
-        //                 rm -f terraform.tfstate*
-        //                 rm -f .terraform.lock.hcl
-
-        //                 terraform init
-        //             """
-        //         }
-        //     }
-        // }
-
-        
-stage('Terraform Init') {
-    steps {
-        withCredentials([
-            [$class: 'AmazonWebServicesCredentialsBinding',
-             credentialsId: 'aws-credentials']
-        ]) {
-
-            sh """
-                cd infra/global/env/${ENVIRONMENT}
-
-                cat > backend.tf <<EOF
-                terraform {
-                  backend "s3" {
-                    bucket         = "terraform-state-bucket-cbz-kharadi-04"
-                    key            = "global/${ENVIRONMENT}/terraform-global.tfstate"
-                    region         = "ap-south-1"
-                    encrypt        = true
-                    dynamodb_table = "terraform-state-lock"
-                    use_lockfile   = true
-                  }
-                }
-                EOF
-
-                rm -rf .terraform
-                rm -f terraform.tfstate*
-                rm -f .terraform.lock.hcl
-
-                terraform init
-            """.stripIndent()
-        }
-    }
+                        cat > backend.tf <<EOF
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-cbz-kharadi-04"
+    key            = "global/${params.ENVIRONMENT}/terraform-global.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+    use_lockfile   = true
+  }
 }
+EOF
 
+                        rm -rf .terraform
+                        rm -f terraform.tfstate*
+                        rm -f .terraform.lock.hcl
+
+                        terraform init
+                    """
+                }
+            }
+        }
 
         stage('TERRAFORM PLAN') {
             steps {
                 withCredentials([
-            [$class: 'AmazonWebServicesCredentialsBinding',
-             credentialsId: 'aws-credentials']
-        ]) {
-                sh '''
-                    cd infra/global/env/${ENVIRONMENT}
+                    [$class: 'AmazonWebServicesCredentialsBinding',
+                     credentialsId: 'aws-credentials']
+                ]) {
 
-                    if [ "$ACTION" = "delete" ]; then
-                        terraform plan -destroy
-                    else
-                        terraform plan
-                    fi
-                '''
+                    sh """
+                        cd infra/global/env/${params.ENVIRONMENT}
+
+                        if [ "${params.ACTION}" = "delete" ]; then
+                            terraform plan -destroy
+                        else
+                            terraform plan
+                        fi
+                    """
+                }
             }
         }
-        
-        }
-
 
         stage('APPROVAL') {
             steps {
-                input message: "Do you want to continue with ${ACTION} for ${ENVIRONMENT}?",
-                      ok: 'APPROVE'
+                input(
+                    message: "Do you want to continue with ${params.ACTION} for ${params.ENVIRONMENT}?",
+                    ok: 'APPROVE'
+                )
             }
         }
-
 
         stage('TERRAFORM APPLY') {
             steps {
                 withCredentials([
-            [$class: 'AmazonWebServicesCredentialsBinding',
-             credentialsId: 'aws-credentials']
-        ]) {
+                    [$class: 'AmazonWebServicesCredentialsBinding',
+                     credentialsId: 'aws-credentials']
+                ]) {
 
-                sh '''
-                    cd infra/global/env/${ENVIRONMENT}
+                    sh """
+                        cd infra/global/env/${params.ENVIRONMENT}
 
-                    if [ "$ACTION" = "delete" ]; then
-                        terraform destroy -auto-approve
-                    else
-                        terraform apply -auto-approve
-                    fi
-                '''
+                        if [ "${params.ACTION}" = "delete" ]; then
+                            terraform destroy -auto-approve
+                        else
+                            terraform apply -auto-approve
+                        fi
+                    """
+                }
             }
         }
-
     }
-    }
-
 
     post {
 
@@ -168,5 +120,7 @@ stage('Terraform Init') {
         }
     }
 }
+
+
 
 
